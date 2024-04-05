c3e6c2789ada5e5501ba3f90bc6c993a199aeaac
Pheeraphat47 <dpheeraphat2547@gmail.com>
2024-04-05 09:59:06 +0700

    first commit

---
+++ demo_borntodev/src/test/java/com/example/demo_borntodev/DemoBorntodevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_borntodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBorntodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
